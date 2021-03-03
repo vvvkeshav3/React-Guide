68b91f67d9464768dbe1c6c7fce18a85bade4bc5
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-03-03 23:16:31 +0530

    Initial Setup of React

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to React App</h1>
+        <p>It is working Fine!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
