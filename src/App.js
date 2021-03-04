0a8f8f9df266c1456781eb131ff7a77b6a7e9214
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-03-04 12:49:27 +0530

    Added Click Event Handler

--- src/App.js
+++ src/App.js
@@ -14,11 +14,23 @@ class App extends Component {
     otherState : 'Some other value'
   }
 
+  switchNameHandler = () =>{
+    this.setState({
+      persons : [
+        {name:'Gopu', age:20},
+        {name:'Gagan', age:19}, 
+        {name:'Mani', age:25},
+        {name:'Nitigya Jain', age:24}
+      ] 
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to React App</h1>
         <p>It is working Fine!</p>
+        <button onClick = {this.switchNameHandler}>Switch Name</button>
         <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
         <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
         <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
@@ -26,7 +38,6 @@ class App extends Component {
       </div>
     );
   }
-  
 }
 
 export default App;
