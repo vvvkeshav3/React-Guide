75e623fe27f6bb7ce96077821146dd1f3fd90948
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-03-04 12:37:32 +0530

    Using props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
 
@@ -8,9 +9,14 @@ class App extends Component {
       <div className="App">
         <h1>Welcome to React App</h1>
         <p>It is working Fine!</p>
+        <Person name = 'Keshav' age = '21'>My Hobby : Playing Chess</Person>
+        <Person name = 'Gagan' age = '19' />
+        <Person name = 'Mani' age = '25' />
+        <Person name = 'Nitigya' age = '24' />
       </div>
     );
   }
+  
 }
 
 export default App;
