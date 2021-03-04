681f85f5cac9ea781a13ed0bdc9542cde2dd2b51
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-03-04 12:43:25 +0530

    Working with State

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import Person from './Person/Person'
 
 class App extends Component {
 
+  state = {
+    persons : [
+      {name:'Keshav', age:21},
+      {name:'Gagan', age:19}, 
+      {name:'Mani', age:25},
+      {name:'Nitigya', age:24}
+    ],
+    otherState : 'Some other value'
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to React App</h1>
         <p>It is working Fine!</p>
-        <Person name = 'Keshav' age = '21'>My Hobby : Playing Chess</Person>
-        <Person name = 'Gagan' age = '19' />
-        <Person name = 'Mani' age = '25' />
-        <Person name = 'Nitigya' age = '24' />
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
+        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
+        <Person name = {this.state.persons[3].name} age = {this.state.persons[3].age}/>
       </div>
     );
   }
