cb0c2d6db27d3579123d97f42c8ae8dd5fa65f64
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-03-08 20:55:55 +0530

    rendering content conditionally (ternary Operator)

--- src/App.js
+++ src/App.js
@@ -11,31 +11,39 @@ class App extends Component {
       {name:'Mani', age:25},
       {name:'Nitigya', age:24}
     ],
+    showPersons : false,
     otherState : 'Some other value'
   }
 
-  switchNameHandler = (newName) =>{
+  // switchNameHandler = (newName) =>{
+  //   this.setState({
+  //     persons : [
+  //       {name: newName, age: 21},
+  //       {name:'Gagan', age:19}, 
+  //       {name:'Mani', age:25},
+  //       {name:'Nitigya', age:24}
+  //     ] 
+  //   });
+  // }
+
+  // nameChangeHandler = (event) =>{
+
+  //   this.setState({
+  //     persons : [
+  //       {name: 'Keshav', age: 21},
+  //       {name: event.target.value, age:19}, 
+  //       {name:'Mani', age:25},
+  //       {name:'Nitigya', age:24}
+  //     ]
+  //   })
+
+  // }
+
+  togglePersonsHandler = () =>{
+    const doesShow = this.state.showPersons;
     this.setState({
-      persons : [
-        {name: newName, age: 21},
-        {name:'Gagan', age:19}, 
-        {name:'Mani', age:25},
-        {name:'Nitigya', age:24}
-      ] 
+      showPersons : !doesShow
     });
-  }
-
-  nameChangeHandler = (event) =>{
-
-    this.setState({
-      persons : [
-        {name: 'Keshav', age: 21},
-        {name: event.target.value, age:19}, 
-        {name:'Mani', age:25},
-        {name:'Nitigya', age:24}
-      ]
-    })
-
   }
 
   render() {
@@ -43,21 +51,20 @@ class App extends Component {
       <div className="App">
         <h1>Welcome to React App</h1>
         <p>It is working Fine!</p>
-        <button onClick = {this.switchNameHandler.bind(this,'Pratham')}>Switch Name</button>
-        {/* <button onClick = {() => this.switchNameHandler('Gopu')}>Switch Name</button>  */}
-        <Person name = {this.state.persons[0].name} 
-                age = {this.state.persons[0].age}  
-                click = {this.switchNameHandler.bind(this,'Gopu')}/>
-
-        <Person name = {this.state.persons[1].name} 
-                age = {this.state.persons[1].age} 
-                changed = {this.nameChangeHandler} />
-                
-        <Person name = {this.state.persons[2].name} 
-                age = {this.state.persons[2].age} />
-                
-        <Person name = {this.state.persons[3].name} 
-                age = {this.state.persons[3].age}/>
+        <button onClick = {this.togglePersonsHandler}>Toggle Persons</button>
+        {
+          this.state.showPersons ? 
+            <div>
+              <Person name = {this.state.persons[0].name} 
+                      age = {this.state.persons[0].age} />
+              <Person name = {this.state.persons[1].name} 
+                      age = {this.state.persons[1].age} />
+              <Person name = {this.state.persons[2].name} 
+                      age = {this.state.persons[2].age} />
+              <Person name = {this.state.persons[3].name} 
+                      age = {this.state.persons[3].age}/>
+            </div> : null
+        }
       </div>
     );
   }
