750003b78f57d3e4980a66797ad5350e53cbf524
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-03-11 23:07:39 +0530

    setting styles dynamically(without any library)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
-
   state = {
-    persons : [
-      {id: 'dasda3', name:'Keshav', age:21},
-      {id: 'huifh21', name:'Gagan', age:19}, 
-      {id: 'nbdfjve', name:'Mani', age:25},
-      {id: 'poifj3', name:'Nitigya', age:24}
+    persons: [
+      { id: 'dasda3', name: 'Keshav', age: 21 },
+      { id: 'huifh21', name: 'Gagan', age: 19 },
+      { id: 'nbdfjve', name: 'Mani', age: 25 },
+      { id: 'poifj3', name: 'Nitigya', age: 24 },
     ],
-    showPersons : false,
-    otherState : 'Some other value'
-  }
+    showPersons: false,
+    otherState: 'Some other value',
+  };
 
-  // switchNameHandler = (newName) =>{
-  //   this.setState({
-  //     persons : [
-  //       {name: newName, age: 21},
-  //       {name:'Gagan', age:19}, 
-  //       {name:'Mani', age:25},
-  //       {name:'Nitigya', age:24}
-  //     ] 
-  //   });
-  // }
-
-  nameChangeHandler = (event,id) =>{
-
-    const personIndex = this.state.persons.findIndex(p => {
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
       return p.id === id;
-    })
+    });
 
     const person = {
-      ...this.state.persons[personIndex]
+      ...this.state.persons[personIndex],
     };
     person.name = event.target.value;
 
@@ -41,49 +28,87 @@ class App extends Component {
     persons[personIndex] = person;
 
     this.setState({
-      persons : persons
+      persons: persons,
     });
+  };
 
-  }
-
-  togglePersonsHandler = () =>{
+  togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons : !doesShow});
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
-  deletePersonsHandler = (personIndex) =>{
+  deletePersonsHandler = (personIndex) => {
     const persons = [...this.state.persons];
-    persons.splice(personIndex,1);
+    persons.splice(personIndex, 1);
     this.setState({
-      persons : persons
+      persons: persons,
     });
-
-  }
+  };
 
   render() {
 
+    const style = {
+      backgroundColor : 'green',
+      color: 'white',
+      font : 'inherit',
+      border: '1px solid blue',
+      padding : '8px',
+      cursor : 'pointer',
+
+      // We can't add hover in inline styles
+      // because if we add class/id to get hover effect from css file
+      // then same properties will not be hover because inline styles will 
+      // override them
+    }
+    // Dynamically Adding Classes For Button
+    const classesButton = ['notUsed'];
+
     let persons = null;
-    if(this.state.showPersons){
+    
+    if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person,index) =>{
-            return <Person 
-                      name = {person.name}
-                      age = {person.age} 
-                      click = {() => this.deletePersonsHandler(index)}
-                      key = {person.id}
-                      changed = {(event)=> this.nameChangeHandler(event,person.id)}
-                      />
-          }) }
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonsHandler(index)}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
+            );
+          })}
         </div>
-      ); 
+      );
+      
+      // Changing Styles Dynamically
+      // Using Inline Styles We can easily change styles dynamically
+      style.backgroundColor = 'red';
+      classesButton[0] = 'used';
     }
 
+  // Adding classes dynamically for paragraph
+    /*
+    Conditions are :
+    1) if number of persons are greater than 2 then no styling on p
+    2) if number of persons are less than or equal to 2 then para to be red 
+    1) if number of persons are less than or equal to 1 then para to be red and bold
+    */
+    const classesPara = ['testing'];
+    if(this.state.persons.length<=2){
+      classesPara.push('red');
+    }
+    if(this.state.persons.length<=1){
+      classesPara.push('bold');
+    }
     return (
       <div className="App">
         <h1>Welcome to React App</h1>
-        <p>It is working Fine!</p>
-        <button onClick = {this.togglePersonsHandler}>Toggle Persons</button>
+        <p className = {classesPara.join(' ')}>It is working Fine!</p>
+        <button className={classesButton.join('')}
+                style = {style}
+                onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
