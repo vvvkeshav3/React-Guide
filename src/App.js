6c0509c2bed91f7d22b08fee5f4aa4fa115ff268
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-03-12 18:19:35 +0530

    Removed styled-components library

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import styled from 'styled-components';
-
-// All styled Components should be defined outside other components
-const StyledButton = styled.button`
-  background-color : ${props => props.alt? 'red' : 'green'};
-  color: white;
-  font : inherit;
-  border: 1px solid blue;
-  padding : 8px;
-  cursor : pointer;
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen' };
-    color: black; 
-  }
-`;
-
 
 class App extends Component {
   state = {
@@ -29,7 +13,6 @@ class App extends Component {
     showPersons: false,
     otherState: 'Some other value',
   };
-
 
   nameChangeHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex((p) => {
@@ -108,12 +91,11 @@ class App extends Component {
       <div className="App">
         <h1>Welcome to React App</h1>
         <p className = {classesPara.join(' ')}>It is working Fine!</p>
-        <StyledButton alt = {this.state.showPersons}
-                      onClick={this.togglePersonsHandler}>Toggle Persons
-        </StyledButton>
+        <button className = "button"
+                onClick={this.togglePersonsHandler}>Toggle Persons
+        </button>
         {persons}
       </div>
-    
     );
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
-// import './Person.css';
+import './Person.css';
 
-// All styled Components should be defined outside other components
-const StyledDiv = styled.div`
-    width : 60%;
-    border : 1px solid rgb(116, 113, 113);
-    box-shadow: 5px 5px 3px rgb(90, 87, 87);
-    margin: 16px auto;
-    padding : 16px;
-    
-    @media (min-width: 700px) {
-          width: 450px;
-    }
-  `;
-  
 const person = (props) => {
   
   return (
-    <StyledDiv className="Person">
+    <div className="Person">
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </StyledDiv>
+    </div>
   );
 };
 
