0ebdb74632720b1c38266412e1ffadc2a9182050
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-03-24 00:21:44 +0530

    Rendering Adjacent JSX Elements using React.Fragment(Built in Aux.js)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 // import classes from './Person.css';
-import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component {
   render(){
     console.log('[Person.js] rendering...');    
     return (
-      <Aux>
+      <Fragment>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old
         </p>
         <p>{this.props.children}</p>,
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </Aux>
+      </Fragment>
     );
   }
 };
