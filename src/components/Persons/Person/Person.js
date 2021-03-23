99a12be86bf5680a3c7b6e224ff415bc7f132d1a
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-03-24 00:20:18 +0530

    Rendering adjacent JSX Elements using higher order components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
 // import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component {
   render(){
     console.log('[Person.js] rendering...');    
-    return [
-        <p onClick={this.props.click} key="i1">
+    return (
+      <Aux>
+        <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old
-        </p>,
-        <p key="i2">{this.props.children}</p>,
-        <input key = "i3" type="text" onChange={this.props.changed} value={this.props.name} />
-    ];
+        </p>
+        <p>{this.props.children}</p>,
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </Aux>
+    );
   }
 };
 
