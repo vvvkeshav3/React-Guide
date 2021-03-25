a282a2bfb5987b2a73a07651779781dde4e1c858
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-03-26 00:18:01 +0530

    One way of creating Higher Order Component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 class App extends Component {
 
   // Steps of Component Lifecycle Creation
@@ -102,7 +103,7 @@ class App extends Component {
 
   
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick= {
           ()=>{
             this.setState({showCockpit: false})
@@ -116,7 +117,7 @@ class App extends Component {
           clicked = {this.togglePersonsHandler}
         />) : null}
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
